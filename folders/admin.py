d19ce79924b5a398a0fc9344ce2b25332760ac55
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-09-10 00:45:42 +0800

    feat: added related name for folder model

--- folders/admin.py
+++ folders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import FolderModel
 # Register your models here.
+
+admin.site.register(FolderModel)
+
--- folders/models.py
+++ folders/models.py
@@ -6,7 +6,7 @@ import random
 
 # Create your models here.
 class FolderModel(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='folders')
     code = models.CharField(max_length=8, unique=True, blank=True)
     subject_name = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
