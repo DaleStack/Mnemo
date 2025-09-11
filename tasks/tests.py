916c60240787d59d1be571772315ffdcd560e0a2
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-09-11 23:02:48 +0800

    test: notification model

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import TaskModel, CommentModel
+from .models import TaskModel, CommentModel, UserEmailNotification
 from folders.models import FolderModel
 
 User = get_user_model()
@@ -72,3 +72,41 @@ class CommentModelTest(TestCase):
             content=content_text
         )
         self.assertEqual(str(comment), content_text[:20])
+
+class UserEmailNotificationTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.folder = FolderModel.objects.create(
+            user=self.user,
+            subject_name='Mathematics'
+        )
+        self.task = TaskModel.objects.create(
+            folder=self.folder,
+            title='Read Chapter 1',
+            created_by=self.user
+        )
+
+    def test_create_user_email_notification(self):
+        notif = UserEmailNotification.objects.create(
+            user=self.user,
+            task=self.task,
+            reminder_sent=True,
+            deadline_sent=False
+        )
+
+        # Ensure notification is created
+        self.assertIsNotNone(notif.id)
+        self.assertEqual(notif.user, self.user)
+        self.assertEqual(notif.task, self.task)
+        self.assertTrue(notif.reminder_sent)
+        self.assertFalse(notif.deadline_sent)
+        self.assertIsNotNone(notif.reminder_sent_at)
+        self.assertIsNotNone(notif.deadline_sent_at)
+
+    def test_str_method(self):
+        notif = UserEmailNotification.objects.create(
+            user=self.user,
+            task=self.task
+        )
+        expected_str = f"Notifications for {self.user.username} on task {self.task.title}"
+        self.assertEqual(str(notif), expected_str)
