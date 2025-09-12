237530eb77a6c89f2dfd23c4e994adf9b6ed7bc6
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-09-13 00:40:25 +0800

    feat: user login function

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import UserModel
 
 class UserCreationForm(UserCreationForm):
     class Meta:
         model = UserModel
         fields = ['username', 'email']
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
-from .forms import UserCreationForm
-from django.contrib.auth import login
+from .forms import UserCreationForm, UserLoginForm
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 # Create your views here.
 
@@ -18,5 +18,23 @@ def user_register(request):
             messages.error(request, 'Unsuccessful Registration')
     else:
         form = UserCreationForm()
+
+    return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Welcome back!')
+                return redirect('home') 
+            else:
+                messages.error(request, 'Unsuccessful Authentication')
+    else:
+        form = UserLoginForm()
 
     return render(request, 'users/login.html', {'form': form})
