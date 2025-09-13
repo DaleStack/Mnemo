87f3045d8c01c2417b126df8ca269f625405a308
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-09-13 22:11:23 +0800

    feat: joining folder functionality

--- folders/urls.py
+++ folders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, create_folder
+from .views import home_page, create_folder, join_folder
 
 urlpatterns = [
     path('home/', home_page, name='home'),
     path('create-folder/', create_folder, name='create_folder'),
+    path('join-folder/', join_folder, name='join_folder'),
 ]
--- folders/views.py
+++ folders/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 @login_required
 def home_page(request):
     user = request.user
-    folders = FolderModel.objects.filter(user=user)
+    folders = FolderModel.objects.filter(members__user=user).distinct()
     return render(request, 'folders/home.html', {'user':user, 'folders': folders})
 
 @login_required
@@ -34,6 +34,30 @@ def create_folder(request):
     else:
         return redirect('home')
 
-    
+@login_required
+def join_folder(request):
+    if request.method == 'POST':
+        code = request.POST.get('code')  # get the code from a form input
+        try:
+            folder = FolderModel.objects.get(code=code)
+        except FolderModel.DoesNotExist:
+            messages.error(request, 'Invalid folder code')
+            return redirect('home')
+
+        # prevent joining twice
+        if FolderMember.objects.filter(folder=folder, user=request.user).exists():
+            messages.info(request, 'You are already a member of this folder')
+            return redirect('home')
+
+        FolderMember.objects.create(
+            folder=folder,
+            user=request.user,
+            role='member'
+        )
+        messages.success(request, f'Joined folder: {folder.subject_name}')
+        return redirect('home')
+
+    else:
+        return redirect('home')
     
                   
