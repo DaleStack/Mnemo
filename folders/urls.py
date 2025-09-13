bbb5c0a980e4706590272bc02839aac9ea73725b
DaleStack <211448639+DaleStack@users.noreply.github.com>
2025-09-13 21:34:09 +0800

    feat: creating folder done

--- folders/urls.py
+++ folders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, create_folder
 
 urlpatterns = [
-    path('home/', home_page, name='home')
+    path('home/', home_page, name='home'),
+    path('create-folder/', create_folder, name='create_folder'),
 ]
